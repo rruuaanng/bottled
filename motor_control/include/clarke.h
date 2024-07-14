a00b9d02cc0f52bade7470e7f10fa6603dbc1fa1
ruang <longjinyii@outlook.com>
2024-07-14 19:04:28 +0800

    ohhh

--- motor_control/include/clarke.h
+++ motor_control/include/clarke.h
@@ -9,7 +9,7 @@
 void clarke_direct_3p_q_fixed(alpha_beta *ab, phase_v v)
 {
     float x0, x1;
-    const int32_t sqrt3 = 56756; // q15
+    const int32_t sqrt3 = 0xDDB4; // q15
     
     ab->alpha = v.A;
     x0 = (sqrt3 * v.A) / 3;
@@ -23,7 +23,7 @@ void clarke_direct_3p_q_fixed(alpha_beta *ab, phase_v v)
 static inline
 void clarke_inverse_2p_q_fixed(phase_v *v, alpha_beta ab)
 {
-    const int32_t sqrt3 = 56756; // q15
+    const int32_t sqrt3 = 0xDDB4; // q15
 
     v->A = ab.alpha;
     v->B = (0 - ab.alpha + sqrt3 * ab.beta) / 2;
