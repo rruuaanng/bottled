3ec1818c99ef642fe726707ad345424b66c3c474
ruang <longjinyii@outlook.com>
2024-07-23 13:02:06 +0800

    move to motor_control dir

---
+++ motor_control/clarke.h
@@ -0,0 +1,45 @@
+#ifndef NONGFU_SPRING_MOTOR_CONTROL_CLARKE_H
+#define NONGFU_SPRING_MOTOR_CONTROL_CLARKE_H
+
+#include <motor_control/common.h>
+
+// 
+// clarke transform
+//
+// assuming three-phase balance, then A+B+C=0
+// 1. alpha = Ia - 1/2Ib - 1/2Ic
+// 2. due to B=C, then 1/2Ib - 1/2Ic = 0
+// 3. alpha = Ia - 0
+// 4. alpha = Ia
+//
+// 1. beta = 2/3 * (sqrt(3)/2Ib - sqrt(3)/2Ic)
+// 2. A+B+C=0, then C=-A-B
+// 3. beta = 2/3 * (sqrt(3)/2Ib + sqrt(3)/2IA + sqrt(3)/2Ib)
+// 4. beta = 2/3 * (sqrt(3)/2Ia + sqrt(3)Ib)
+// 5. beta = sqrt(3)/3Ia + 2sqrt(3)/3Ib
+static inline
+void clarke_direct_3p_q_fixed(alpha_beta *ab, phase_v v)
+{
+    float x0, x1;
+    const int32_t sqrt3 = 0xDDB4; // q15
+    
+    ab->alpha = v.ia;
+    x0 = (sqrt3 * v.ia) / 3;
+    x1 = (2 * sqrt3 * v.ib) / 3;
+    ab->beta = x0 + x1;
+}
+
+//
+// clarke inverse transform
+//
+static inline
+void clarke_inverse_2p_q_fixed(phase_v *v, alpha_beta ab)
+{
+    const int32_t sqrt3 = 0xDDB4; // q15
+
+    v->ia = ab.alpha;
+    v->ib = (0 - ab.alpha + sqrt3 * ab.beta) / 2;
+    v->ic = (0 - ab.alpha - sqrt3 * ab.beta) / 2;
+}
+
+#endif // NONGFU_SPRING_MOTOR_CONTROL_CLARKE_H
---
+++ motor_control/park.h
@@ -0,0 +1,28 @@
+#ifndef NONGFU_SPRING_MOTOR_CONTROL_PARK_H
+#define NONGFU_SPRING_MOTOR_CONTROL_PARK_H
+
+#include <motor_control/common.h>
+
+//
+// park transform
+//
+static inline
+void park_direct_2p_q_fixed(
+    q_d *qd, alpha_beta ab,
+    int32_t sin_theta, int32_t cos_theta)
+{
+
+}
+
+//
+// park inverse transform
+//
+static inline
+void park_inverse_2p_q_fixed(
+    alpha_beta *ab, q_d qd,
+    int32_t sin_theta, int32_t cos_theta)
+{
+    
+}
+
+#endif // NONGFU_SPRING_MOTOR_CONTROL_PARK_H
