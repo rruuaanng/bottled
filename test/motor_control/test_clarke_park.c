5ae0dba5357e93a1d0911b5e6a081b24c6f31062
ruang <longjinyii@outlook.com>
2024-08-06 20:06:15 +0800

    [file] test_clarke.c rename to test_clarke_park.c

--- test/motor_control/test_clarke_park.c
+++ test/motor_control/test_clarke_park.c
@@ -1,4 +1,5 @@
 #include <motor_control/clarke.h>
+#include <motor_control/park.h>
 #include <stdio.h>
 
 
@@ -19,14 +20,25 @@ int main(int argc, char const *argv[])
 
     clarke_direct_3p_q15(&ab, i);
     printf("clarke -> alpha: %.4f \t beta: %.4f\n",
-            q15_fixed_to_float(ab.alpha),
-            q15_fixed_to_float(ab.beta));
+        q15_fixed_to_float(ab.alpha),
+        q15_fixed_to_float(ab.beta));
+
+    park_direct_2p(&qd, ab, 30);
+    printf("park -> q: %.4f \t d: %.4f\n",
+        q15_fixed_to_float(qd.q),
+        q15_fixed_to_float(qd.d));
+
+    park_inverse_2p(&ab, qd, 30);
+    printf("park inverse -> alpha: %.4f \t beta: %.4f\n",
+        q15_fixed_to_float(ab.alpha),
+        q15_fixed_to_float(ab.beta));
 
     clarke_inverse_2p_q15(&i, ab);
     printf("clarke inverse -> A: %.4f \t B: %.4f \t C: %.4f \n",
             q15_fixed_to_float(i.ia),
             q15_fixed_to_float(i.ib),
             q15_fixed_to_float(i.ic));
+
 
     return 0;
 }
