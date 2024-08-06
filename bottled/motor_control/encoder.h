5378d358760a262ce1f8186ece110bf91db92949
ruang <longjinyii@outlook.com>
2024-08-06 13:41:46 +0800

    [file] added encoder desc structure

--- bottled/motor_control/encoder.h
+++ bottled/motor_control/encoder.h
@@ -5,6 +5,28 @@
 extern "C" {
 #endif
 
+#include <common/types.h>
+
+struct encoder_desc {
+    char type;
+#define ENCODER_DESC_TYPE_HALL              'H'
+#define ENCODER_DESC_TYPE_INCREMENT         'I'
+
+    u16 resolution;
+    u64 timestamp;
+
+    u16 current_angle;  
+    u16 last_angle;
+    u16 angle_range_end;
+    u16 zero_point_pos;
+
+    u64 current_cnt; // clear to zero when reaching max_cnt_end
+    u64 last_cnt;   
+    u64 max_cnt_end;
+
+    void (*current_cnt_handle)(struct encoder_desc *);
+};
+
 #ifdef __cplusplus
 }
 #endif 
