c8e8e30ccd737704a14b35de0eb7381a701eb278
ruang <longjinyii@outlook.com>
2024-08-06 11:35:52 +0800

    [file] added types.h

---
+++ bottled/common/types.h
@@ -0,0 +1,37 @@
+
+#ifndef NONGFU_SPRING_COMMON_TYPES_H
+#define NONGFU_SPRING_COMMON_TYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//
+// signed
+//
+typedef int8_t              i8;
+typedef int16_t             i16;
+typedef int32_t             i32;
+typedef int64_t             i64;
+
+//
+// unsigned
+//
+typedef uint8_t             u8;
+typedef uint16_t            u16;
+typedef uint32_t            u32;
+typedef uint64_t            u64;
+
+typedef uint8_t             byte;
+typedef uint16_t            word;
+typedef uint32_t            dword;
+
+#ifdef __cplusplus
+}
+#endif 
+
+#endif // NONGFU_SPRING_COMMON_TYPES_H
+
