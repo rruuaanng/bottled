543ecccf2bf480425df90149b4c0ee7e809d1bfa
ruang <longjinyii@outlook.com>
2024-08-06 19:20:08 +0800

    [file] added clarke.h test file

---
+++ test/motor_control/test_clarke.c
@@ -0,0 +1,32 @@
+#include <motor_control/clarke.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    alpha_beta ab;
+    q_d qd;
+
+    int wf = 15;
+    q_fixed_fvar(x1, 25.15, wf);
+    q_fixed_fvar(x2, 24.1851, wf);
+    phase_i_var(i, x1, x2, 0);
+
+    printf("A: %.4f \t B: %.4f \t C: %.4f\n",
+                q15_fixed_to_float(i.ia),
+                q15_fixed_to_float(i.ib),
+                q15_fixed_to_float(i.ic));
+
+    clarke_direct_3p_q15(&ab, i);
+    printf("clarke -> alpha: %.4f \t beta: %.4f\n",
+            q15_fixed_to_float(ab.alpha),
+            q15_fixed_to_float(ab.beta));
+
+    clarke_inverse_2p_q15(&i, ab);
+    printf("clarke inverse -> A: %.4f \t B: %.4f \t C: %.4f \n",
+            q15_fixed_to_float(i.ia),
+            q15_fixed_to_float(i.ib),
+            q15_fixed_to_float(i.ic));
+
+    return 0;
+}
