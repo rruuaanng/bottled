579dfd863972f7ba1da9d563405bf298ee768d27
ruang <longjinyii@outlook.com>
2024-08-24 20:41:48 +0800

    [file] added bottled.h

---
+++ bottled/bottled.h
@@ -0,0 +1,22 @@
+#ifndef BOTTLED_H
+#define BOTTLED_H
+
+//
+// import common module
+//
+#include <common/bit.h>
+#include <common/qfixed.h>
+#include <common/types.h>
+
+//
+// import motor_control header file
+//
+#include <motor_control/clarke.h>
+#include <motor_control/park.h>
+#include <motor_control/encoder.h>
+#include <motor_control/common.h>
+#include <motor_control/pid_control.h>
+#include <motor_control/svm.h>
+#include <motor_control/observer/kalman_filter.h>
+
+#endif // BOTTLED_H
